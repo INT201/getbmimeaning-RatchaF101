88480325e69c8a4462bfd86cc74afe016c0b7bb3
Ratchapon <ratchapon.sang@kmutt.ac.th>
2022-08-31 00:50:39 +0700

    reslove Hw02

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -8,13 +8,13 @@ function calculateBMI(weight, height) {
 function getBMIMeaning(weight, height) {
   calculateBMI(weight,height)
   if(BMI < 18.5){
-      return console.log("Underweight")
+      return "Underweight"
   }
   else if(18.5 < BMI && BMI < 24.9){
-      return console.log("Normal weight")
+      return "Normal weight"
   }
   else if(BMI > 25.0){
-      return console.log("Overweight")
+      return "Overweight"
   }else{
       return false
   }
