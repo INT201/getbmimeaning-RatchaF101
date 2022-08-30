88a60e3ec8b37e605ed1ff79d8741bd08ffd7e65
Ratchapon <ratchapon.sang@kmutt.ac.th>
2022-08-31 00:48:31 +0700

    sent Hw02

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,22 @@
 const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  BMI = weight/Math.pow(height,2)
+  console.log(BMI)
 }
+
 function getBMIMeaning(weight, height) {
-  //code here
+  calculateBMI(weight,height)
+  if(BMI < 18.5){
+      return console.log("Underweight")
+  }
+  else if(18.5 < BMI && BMI < 24.9){
+      return console.log("Normal weight")
+  }
+  else if(BMI > 25.0){
+      return console.log("Overweight")
+  }else{
+      return false
+  }
 }
 module.exports = getBMIMeaning
